b5a6f8d1d89912295a60b74ca2d9fb3439f0fdcc
Luís Passos <luis.passos013@gmail.com>
2022-01-11 11:35:04 -0300

    add updateBalance function

---
+++ scripts.js
@@ -0,0 +1,97 @@
+const Modal = {
+    toggle() {
+        document.querySelector('.modal-overlay').classList.toggle('active');
+    }
+}
+
+const transactions = [{
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: -5000000,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -200000,
+        date: '23/01/2021',
+    },
+    {
+        id: 4,
+        description: 'App',
+        amount: 200000,
+        date: '23/01/2021',
+    },
+]
+
+const Transaction = {
+    incomes() {
+
+    },
+    expenses() {
+
+    },
+    total() {
+
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr);
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+            <tr>
+                <td class="description">${transaction.description}</td>
+                <td class="${CSSclass}">${amount}</td>
+                <td class="date">${transaction.date}</td>
+                <td>
+                    <img src="assets/minus.svg" alt="Remover Transação">
+                </td>
+            </tr>
+        `
+
+        return html
+    },
+    updateBalance() {
+        document.getElementById('incomeDisplay')
+        .innerHTML = "Discover"
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+transactions.forEach(transaction => {
+    DOM.addTransaction(transaction)
+})
